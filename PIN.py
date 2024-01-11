1c3309315c1bd8bc3724ffde609398fe30d28f17
David Richey <davidsacc@gmail.com>
2024-01-11 12:45:09 -0500

    Update PIN.py

--- PIN.py
+++ PIN.py
@@ -1,5 +1,22 @@
-![PIN](https://github.com/StudentLoans999/Python/assets/77641113/a7a3f39c-ac1c-48f0-b4b5-c51ee0580f62)
+Please write a program which keeps asking the user for a PIN code until they type in the correct one, which is 4321. The program should then print out the number of times the user tried different codes.
 
+Sample output
+PIN: 3245
+Wrong
+PIN: 1234
+Wrong
+PIN: 0000
+Wrong
+PIN: 4321
+Correct! It took you 4 attempts
+
+If the user gets it right on the first try, the program should print out something a bit different:
+
+Sample output
+PIN: 4321
+Correct! It only took you one single attempt!
+
+# Write your solution here
 attempts = 0 # initialize variable
 
 while True: # this loop runs till the user inputs the correct PIN (4321)
