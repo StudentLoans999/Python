ccf5fa63a6881411554b22257d31b64ae6b25143
David Richey <davidsacc@gmail.com>
2024-01-14 13:24:32 -0500

    Create Width.py

---
+++ Width.py
@@ -0,0 +1,5 @@
+# Write your solution here
+width = int(input("Width: "))
+characters = "#" # character to be outputted
+characters *= width # determines how many 'characters' to output, which is 'width'
+print(f"{characters}")
