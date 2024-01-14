e1ce3f03396374360d754537952153f8691a05bc
David Richey <davidsacc@gmail.com>
2024-01-14 17:01:09 -0500

    Update CharacterMatch.py

--- CharacterMatch.py
+++ CharacterMatch.py
@@ -1,3 +1,15 @@
+Please write a program which asks the user for a string. The program then prints out a message based on whether the second character and the second to last character are the same or not. See the examples below.
+
+Sample output
+Please type in a string: python
+The second and the second to last characters are different
+
+Sample output
+Please type in a string: pascal
+The second and the second to last characters are a
+
+# Write your solution here
+
 word = input("Please type in a string: ")
 
 if len(word) > 1 and word[1] == word[-2]: # if at least two characters, and second character matches second to last character
