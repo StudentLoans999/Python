3c349a7e0332a177cd9a76e47e3c6d80efb5cd25
David Richey <davidsacc@gmail.com>
2024-01-14 17:03:00 -0500

    Update HeightAndWidth.py

--- HeightAndWidth.py
+++ HeightAndWidth.py
@@ -1,3 +1,12 @@
+Please modify the previous program (Width.py) so that it also asks for the height, and prints out a rectangle of hash characters accordingly.
+
+Sample output
+Width: 10
+Height: 3
+##########
+##########
+##########
+
 # Write your solution here
 width = int(input("Width: "))
 height = int(input("Height: "))
