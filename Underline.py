cdba4751f4298d2889f17055042e88a150f1fd0f
David Richey <davidsacc@gmail.com>
2024-01-14 16:54:20 -0500

    Update Underline.py

--- Underline.py
+++ Underline.py
@@ -1,13 +1,13 @@
 # Write your solution here
-characters = "-"
+characters = "-" # character to be outputted
 
 while True: # keeps running until broken
     string = input("Please type in a string: ")
     print()
     print(string)
     index = len(string)
-    print(f"{characters*index}")
+    print(f"{characters*index}") # determines how many 'characters' to output, which is 'index'
     print()
 
-    if string == "":
+    if string == "": # exits the loop when user inputs an empty string (presses Enter)
         break
