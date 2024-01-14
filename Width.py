f0bb789272889d24922f4522ad609d06927d55c5
David Richey <davidsacc@gmail.com>
2024-01-14 17:02:16 -0500

    Update Width.py

--- Width.py
+++ Width.py
@@ -1,3 +1,14 @@
+Please write a program which prints out a line of hash characters, the width of which is chosen by the user.
+
+Sample output
+Width: 3
+
+###
+Sample output
+Width: 8
+
+########
+
 # Write your solution here
 width = int(input("Width: "))
 characters = "#" # character to be outputted
