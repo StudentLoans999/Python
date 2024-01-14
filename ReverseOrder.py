68754de1dbbc8a2f4fc1330cca1cccaa820503a4
David Richey <davidsacc@gmail.com>
2024-01-14 13:10:21 -0500

    Update ReverseOrder.py

--- ReverseOrder.py
+++ ReverseOrder.py
@@ -1,6 +1,7 @@
-
+# Write your solution here
 input_string = input("Please type in a string: ")
 index = -1
-while index >= -len(input_string):
-    print(input_string[index])
-    index -= 1
+
+while index >= -len(input_string): # loops exactly the amount of characters in the string
+    print(input_string[index]) # gets the character at the last position in the string (first run)
+    index -= 1 # for second run, the character is now the second to last position ... and so on
