f08fee8f59dab790dd4df0b71f26d73bfd3e5f60
David Richey <davidsacc@gmail.com>
2024-01-14 16:58:52 -0500

    Update ConsecutiveSum.py

--- ConsecutiveSum.py
+++ ConsecutiveSum.py
@@ -1,3 +1,17 @@
+Please write a program which asks the user to type in a limit. The program then calculates the sum of consecutive numbers (1 + 2 + 3 + ...) until the sum is at least equal to the limit set by the user. In addition to the result it should also print out the calculation performed:
+
+Sample output
+Limit: 2
+The consecutive sum: 1 + 2 = 3
+
+Sample output
+Limit: 10
+The consecutive sum: 1 + 2 + 3 + 4 = 10
+
+Sample output
+Limit: 18
+The consecutive sum: 1 + 2 + 3 + 4 + 5 + 6 = 21
+
 # Write your solution here
 
 # Initialize variables
