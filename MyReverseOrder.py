09926b2d190a41330f9536fdd073448d522bf49a
David Richey <davidsacc@gmail.com>
2024-01-14 17:00:13 -0500

    Update MyReverseOrder.py

--- MyReverseOrder.py
+++ MyReverseOrder.py
@@ -1,3 +1,14 @@
+Please write a program which asks the user for a string. The program then prints out the input string in reversed order, from end to beginning. Each character should be on a separate line.
+
+An example of expected behaviour:
+
+Sample output
+Please type in a string: hiya
+a
+y
+i
+h
+
 # Write your solution here
 input_string = input("Please type in a string: ")
 index = 0
