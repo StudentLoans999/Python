3621a86a4cd57f3a4fe877bfba4cbaab96b7954b
David Richey <davidsacc@gmail.com>
2024-01-14 13:29:22 -0500

    Update HeightAndWidth.py

--- HeightAndWidth.py
+++ HeightAndWidth.py
@@ -1,9 +1,9 @@
 # Write your solution here
 width = int(input("Width: "))
 height = int(input("Height: "))
-characters = "#"
-characters *= width # 
+characters = "#" # character to be outputted
+characters *= width # determines how many 'characters' to output, which is 'width'
 
-while height > 0:
+while height > 0: # loops 'height' times (so will make this many rows)
     print(f"{characters}")
     height -= 1
