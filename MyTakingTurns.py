6350503a0558bb7cfb48bb7aa9b26fece907388a
David Richey <davidsacc@gmail.com>
2024-01-17 15:35:08 -0500

    Update MyTakingTurns.py

--- MyTakingTurns.py
+++ MyTakingTurns.py
@@ -22,13 +22,13 @@ number = int(input("Please type in a number: "))
 first_integer = 1 # initialize the first integer
 second_integer = number # initialize the second integer to be 'number'
 
-while first_integer <= second_integer or second_integer > first_integer: #
+while first_integer <= second_integer or second_integer > first_integer: # loops until the first number is larger than 'number' or until the second number is less than the first number
 
-    if first_integer <= number:
+    if first_integer <= number: # checks if first number is less than or equal to 'number' and outputs first number if so
         print(first_integer)
 
-    if second_integer > first_integer:
+    if second_integer > first_integer: # checks if second number is greater than first number and outputs second number if so
         print(second_integer)
 
-    first_integer += 1
-    second_integer -= 1
+    first_integer += 1 # iterates through the loop to get closer to reaching 'number'
+    second_integer -= 1 # iterates through the loop to get further from 'number'
