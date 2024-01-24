238167541ea2c13126756465087c5a62e1d09e49
David Richey <davidsacc@gmail.com>
2024-01-23 23:32:42 -0500

    Update Spruce.py

--- Spruce.py
+++ Spruce.py
@@ -30,32 +30,22 @@ def line(length, string): # first function
 
 
 # Write your solution here
-def shape(triangle_height, triangle_character, rectangle_height, rectangle_character): # define function and assign it the parameter of 'triangle_height'
-    triangle_rows = triangle_height # initialize new variable by assigning it to the parameter 'triangle_height'
-    triangle_width = 1                                                                                                                                                  
+def spruce(height): # define function and assign it the parameter of 'height'
+    rows = height # initialize new variable by assigning it to the parameter 'height'
+    width = 1                                                                                                                                                  
                                                                                                                                                   
-    while triangle_rows > 0: # loop until there are the number of rows input in the parameter 'triangle_height'
+    while rows > 0: # loop until there are the number of rows input in the parameter 'height'
+      character = '#' # initialize new variable that will be output and will create the triangle shape
 
-      while triangle_width <= triangle_height:
+      while width <= height:
         
-        print(f"{triangle_character*triangle_width}") # output the 'triangle_character' 'triangle_width' times
-        triangle_width += 1
+        print(f"{character*width}") # output the 'character' 'width' times
+        width += 1
         
-      triangle_rows -= 1 # iterates so that there are 'triangle_height' number of rows outputted
-
-    rectangle_rows = rectangle_height # initialize new variable by assigning it to the parameter 'rectangle_height'
-                                                                                                                                                     
-                                                                                                                                                  
-    while rectangle_rows > 0: # loop until there are the number of rows input in the parameter 'rectangle_height'
-
-      print(f"{rectangle_character*triangle_height}") # output the 'rectangle_character' 'triangle_height' times
-        
-      rectangle_rows -= 1 # iterates so that there are 'rectangle_height' number of rows outputted
+      rows -= 1 # iterates so that there are 'height' number of rows outputted
       
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    shape(5, "X", 3, "*")
+    spruce(3)
     print()
-    shape(2, "o", 4, "+")
-    print()
-    shape(3, ".", 0, ",")                                                                                                                                   
+    spruce(5)      
