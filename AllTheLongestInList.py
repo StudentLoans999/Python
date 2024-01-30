94edf55f5683fb765a83f95c569a38950f408e66
David Richey <davidsacc@gmail.com>
2024-01-30 17:17:46 -0500

    Update AllTheLongestInList.py

--- AllTheLongestInList.py
+++ AllTheLongestInList.py
@@ -11,3 +11,31 @@ my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
 result = all_the_longest(my_list)
 print(result) # ['dorothy', 'richard']
 
+def all_the_longest(my_list: list): # function that returns the length of the shortest string in the given list 
+    longest_length = 0  # initialize (define the criteria wanted) the determining/comparison variable
+    longest_list = [] # initialize an empty list
+
+    for string in my_list: # loop through all the strings in the given list
+        if len(string) >= longest_length: # check if the length of the current element is greater than or equal to the last greatest length (the longest previous string)
+
+            if len(string) > longest_length: # check if the current element's length is greater than the longest length
+
+                longest_length = len(string) # update the variable to be the length of the new element
+                longest_list = [string]  # start a new list with the current element (or reset the new list to have only this one element)
+
+            else:
+                longest_list.append(string) # if the current element's length is equal to the longest length, append it to the new list
+
+    return longest_list # return the list of all the longest strings in the given list
+
+# Testing the function
+if __name__ == "__main__":
+    # First Example
+    my_list = ["first", "second", "eleventh", "fourth"]
+    result = all_the_longest(my_list)
+    print(result) # ['eleventh']
+
+    # Second Example
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = all_the_longest(my_list)
+    print(result) # ['dorothy', 'richard']
