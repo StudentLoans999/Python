3b7bb96961a31a68c645bdd0de3fc1daf97de562
David Richey <davidsacc@gmail.com>
2024-02-12 17:00:22 -0500

    Update NumberOfMatchesIn2DMatrix.py

--- NumberOfMatchesIn2DMatrix.py
+++ NumberOfMatchesIn2DMatrix.py
@@ -1,3 +1,12 @@
+Please write a function named count_matching_elements(my_matrix: list, element: int), which takes a two-dimensional array of integers and a single integer value as its arguments. The function then counts how many elements within the matrix match the argument value.
+
+An example of how the function should work:
+
+m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+print(count_matching_elements(m, 1))
+Sample output
+3
+
 def count_matching_elements(my_matrix: list, element: int): # goes through each element in the 2D matrix and outputs the count of how many of them match the specified target element 
     number_of_element = 0 # initialize count of the element
 
