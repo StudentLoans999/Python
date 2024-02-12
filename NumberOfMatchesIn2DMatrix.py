d1ea963bfe342304ee66c13c17e9b0e2159311ec
David Richey <davidsacc@gmail.com>
2024-02-12 17:20:17 -0500

    Update NumberOfMatchesIn2DMatrix.py

--- NumberOfMatchesIn2DMatrix.py
+++ NumberOfMatchesIn2DMatrix.py
@@ -21,4 +21,4 @@ def count_matching_elements(my_matrix: list, element: int): # goes through each 
 # Testing the function
 if __name__ == "__main__":
     m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
-    print(count_matching_elements(m, 1))
+    print(count_matching_elements(m, 1)) # 3
